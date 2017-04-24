14fbde25284a71a16ffa802abb31c49477424563
Anthony Muisyo <muisyoanthony@gmail.com>
2017-04-24 19:09:09 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,81 @@
+'use strict';
+var NEM = require('./NEM.js');
+var _ = require('lodash');
+var async = require('async');
+var fs = require('fs');
+var path = require('path');
+
+var nem = new NEM(null);
+
+var walletName = null;
+
+async.waterfall([
+    //Get user input
+    (cb) => {
+        var scriptName = path.basename(__filename);
+        if (process.argv.length <= 3) {
+            console.log("USAGE: " + scriptName + " walletname password");
+            process.exit(1);
+        }
+        var conf = {
+            "addressBook": process.argv[2],
+            "password": process.argv[3]
+        };
+        return cb(null, conf);
+    },
+    (conf, cb) => {
+        //Fetch addresses
+        nem.nccPost("/addressbook/open", conf,
+            (err) => {
+                if (err) {
+                    return cb(err);
+                }
+            },
+            (inf) => {
+                walletName = inf.addressBook;
+                var addressObjects = inf.accountLabels;
+                var formattedAddresses = _.map(addressObjects, (add) => {
+                    return {
+                        label: add.privateLabel || add.publicLabel,
+                        address: add.address
+                    };
+                });
+                console.log("Successfully retrieved addresses..")
+                return cb(null, formattedAddresses);
+            }
+        );
+    },
+    (fa, cb) => {
+        //Base64 the formatted addresses
+        var adb = base64Encode(JSON.stringify(fa));
+        console.log("Encoded addresses..");
+        return cb(null, adb);
+    },
+    (adb, cb) => {
+        //Write to file. 
+        fs.writeFile(`./${walletName}.adb`, adb, (err) => {
+            if (err) {
+                return cb(err);
+            }
+            console.log("Successfully written to file..");
+            return cb(null);
+        });
+    }
+], (err) => {
+    if (err) {
+        //err.code || err.errno == "ECONNREFUSED"
+        if (err.code === "ECONNREFUSED" || err.errno === "ECONNREFUSED") {
+            console.log("Please make sure that your NCC instance is running prior to running this tool.");
+            process.exit(1);
+        }
+        throw err;
+    }
+    process.exit(1);
+});
+
+
+
+function base64Encode(addresses) {
+    return new Buffer(addresses).toString('base64');
+    //Error checking?
+}
---
+++ readme.md
@@ -0,0 +1,54 @@
+# NCC to Nanowallet address book exporter
+
+Utility to help in moving addresses from NCC into a readable Nanowallet adb file 
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine.
+
+### Prerequisites
+
+You'll need to ave installed the following:
+
+```
+Node.js
+```
+
+### Installing
+
+
+Clone this repo on to your machine and cd into the repo folder
+
+```
+git clone blah blah
+cd blah blah
+```
+
+Install required packages
+
+```
+npm install
+```
+
+Run the script
+
+```
+node app.js addressbookname password
+```
+
+## Running the tests
+
+TODO
+
+
+## Author
+
+* **Anthony Muisyo** 
+
+## License
+
+This project is licensed under the MIT License
+
+## Acknowledgments
+
+NEM community
